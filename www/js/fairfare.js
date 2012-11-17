cd37e4f500842aeaa1db09535953703dbbe05414
Allejo Chris Velarde <chris.velarde@chromedia.com>
2012-11-18 03:56:21 +0800

    updated server side

--- www/js/fairfare.js
+++ www/js/fairfare.js
@@ -12,6 +12,10 @@ var tracker = {
     defaultBounds : {},
         
     trackedPositions: [],
+    
+    directionsService: new google.maps.DirectionsService(),
+    
+    directionsDisplay: null,
         
     commonLocationOptions: { maximumAge: 3000, timeout: 5000, enableHighAccuracy: true },
     
@@ -26,9 +30,14 @@ var tracker = {
                 
                 gPosition = new google.maps.LatLng(tracker.currentPosition.coords.latitude, tracker.currentPosition.coords.longitude);
                 
+                tracker.directionsDisplay = new google.maps.DirectionsRenderer();
+                
                 tracker.initMapWidgets()
                     .buildMap(gPosition)
-                    .pinOrigin(gPosition);
+                    //.pinOrigin(gPosition)
+                    ;
+                
+                tracker.directionsDisplay.setMap(tracker.map);
             }, // end getCurrentPosition.success callback
             
             error: function(error) {
@@ -65,6 +74,24 @@ var tracker = {
     
     pinDestination: function(position) {
         tracker.destinationMarker = tracker._changeMarker(tracker.destinationMarker, position);
+        
+        tracker.renderDirections();
+    },
+    
+    renderDirections: function() {
+        
+        var start = document.getElementById('origin').value;
+        var end = document.getElementById('destination').value;
+        var request = {
+            origin:start,
+            destination:end,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        };
+        tracker.directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            tracker.directionsDisplay.setDirections(response);
+          }
+        });
     },
     
     _changeMarker: function(marker, position) {
@@ -94,32 +121,32 @@ var tracker = {
         var inputTo = new google.maps.places.Autocomplete(document.getElementById('destination'), {bounds: tracker.defaultBounds});
         
         // bind listener for origin
-        google.maps.event.addListener(inputFrom, 'place_changed', function() {
-            var place = inputFrom.getPlace();
-            if (!place.geometry) {
-              return;
-            }
-            if (place.geometry.viewport) {
-              //tracker.map.fitBounds(place.geometry.viewport);
-            } else {
-              tracker.map.setCenter(place.geometry.location);
-            }
-            tracker.pinOrigin(place.geometry.location);
-          });
-        
-        // bind listener for destination
-        google.maps.event.addListener(inputTo, 'place_changed', function() {
-            var place = inputTo.getPlace();
-            if (!place.geometry) {
-              return;
-            }
-            if (place.geometry.viewport) {
-              //tracker.map.fitBounds(place.geometry.viewport);
-            } else {
-              tracker.map.setCenter(place.geometry.location);
-            }
-            tracker.pinDestination(place.geometry.location)
-          });
+//        google.maps.event.addListener(inputFrom, 'place_changed', function() {
+//            var place = inputFrom.getPlace();
+//            if (!place.geometry) {
+//              return;
+//            }
+//            if (place.geometry.viewport) {
+//              //tracker.map.fitBounds(place.geometry.viewport);
+//            } else {
+//              tracker.map.setCenter(place.geometry.location);
+//            }
+//            tracker.pinOrigin(place.geometry.location);
+//          });
+//        
+//        // bind listener for destination
+//        google.maps.event.addListener(inputTo, 'place_changed', function() {
+//            var place = inputTo.getPlace();
+//            if (!place.geometry) {
+//              return;
+//            }
+//            if (place.geometry.viewport) {
+//              //tracker.map.fitBounds(place.geometry.viewport);
+//            } else {
+//              tracker.map.setCenter(place.geometry.location);
+//            }
+//            tracker.pinDestination(place.geometry.location)
+//          });
 //        
         return this;
     },
